0939ffd670c93ebea887dbb4519201381a7a8179
Alexander Mischuk <mischuk.alexander@gmail.com>
2016-12-15 01:04:53 +0200

    Base template

--- source/modules/header/m_header.js
+++ source/modules/header/m_header.js
@@ -1,1 +1,29 @@
-console.log('Initial header.js');
+function mobileMenu() {
+  var $bgLayout = $('.bg-layout');
+  var $mobileMenuButton = $('.mobile-menu');
+  var $bottomHeader = $('.bottom-header');
+  $mobileMenuButton.on('click', function(){
+    $bottomHeader.toggleClass('is-opened');
+    $bgLayout.toggleClass('is-active');
+  });
+
+  $bgLayout.on('click', function(){
+    $bottomHeader.removeClass('is-opened');
+    $bgLayout.removeClass('is-active');
+  });
+}
+mobileMenu();
+
+function mobileMenuAccordion() {
+  $('.dropdown-menu').on('click', function(){
+    if ( $(this).hasClass('is-active') ) {
+      $(this).removeClass('is-active');
+      return false;
+    } else {
+      $('.dropdown-menu').removeClass('is-active');
+      $(this).addClass('is-active');
+      return false;
+    }
+  });
+}
+mobileMenuAccordion();
